c9b407bbbfc913b0b2502e3ddf3831a045c7a9f6
miko_200 <miko.caky@gmail.com>
2026-09-17 08:55:40 +0200

    Chessboard loads on start

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,24 @@
 "use strict";
+const chessboardContainer = document.getElementById("chessboard");
+onload = () => {
+    console.log("Page loaded. Ready to create chessboard.");
+    createChessboard();
+};
+function createChessboard() {
+    console.log("Creating chessboard...");
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            if ((i + j) % 2 === 0) {
+                tile.classList.add("white-tile");
+            }
+            else {
+                tile.classList.add("black-tile");
+            }
+            tile.setAttribute("data-position", `${i},${j}`);
+            chessboardContainer.appendChild(tile);
+            console.log(`Tile created at position (${tile.getAttribute("data-position")}) with class ${tile.className}`);
+        }
+    }
+}
